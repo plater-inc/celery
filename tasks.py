40699af9f6efdb30154b1b62ea836daa6abc8cab
orr@plater.com <netbkb@gmail.com>
2021-11-11 01:39:02 -0500

    feat: tasks delegate to webhook url

--- tasks.py
+++ tasks.py
@@ -1,9 +1,13 @@
 import os
 from celery import Celery
+import requests
 
 app = Celery('tasks', broker=os.getenv("CELERY_BROKER_URL"))
 
+endpoint = os.getenv("WEBHOOK_URL")
+
 
 @app.task
-def add(x, y):
-    return x + y
+def add(request):
+    res = requests.post(endpoint, request, headers={"Content-Type": "application/json"}) 
+    return res.json()
